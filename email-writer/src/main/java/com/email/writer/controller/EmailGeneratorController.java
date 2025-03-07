5e165d27568ca445d876fd20b2d89ed448c86aea
Aryan Tiwari <aryantiwari522@gmail.com>
2025-03-07 13:13:59 +0530

    first commit

---
+++ email-writer/src/main/java/com/email/writer/controller/EmailGeneratorController.java
@@ -0,0 +1,23 @@
+package com.email.writer.controller;
+
+import com.email.writer.entity.EmailRequest;
+import com.email.writer.service.EmailGeneratorService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/email")
+@CrossOrigin("*")
+public class EmailGeneratorController {
+
+    private final EmailGeneratorService emailGeneratorService;
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest){
+        String response = emailGeneratorService.generateEmailReply(emailRequest);
+        return ResponseEntity.ok(response);
+    }
+}
